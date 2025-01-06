b4fcf59adc6e7a28326dd5a99c4bcc6e5ce76c32
EuijinSeo <97028331+EuijinSeo@users.noreply.github.com>
2025-01-06 14:35:36 +0900

    Update target_strand_ratio_calculation.py

--- target_strand_ratio_calculation.py
+++ target_strand_ratio_calculation.py
@@ -1,6 +1,5 @@
 """
-
-2024-x-xx
+2025-x-xx
 
 Author : Euijin Seo (ufoooo1391@postech.ac.kr)
 
@@ -16,16 +15,19 @@ B: The number of reads that target the template strand in the SAM file
 C: The number of reads in the SAM file
 """
 
+# Obtain the read information from SAM file 
 def sam_mining(x):
     xx = x.split('\t')
     y = [xx[1],int(xx[3]),int(xx[3])+int(xx[5][:-1])-1]
-    return y
+    return y # y[0]:, y[1]: start position, y[2]: end position
 
+# Obtain the read information from GCF file
 def gcf_mining(x):
     xx = x.split('\t')
     y = [xx[2],int(xx[3]),int(xx[4]),xx[-3]]
-    return y
+    return y # y[0]:, y[1]: start position, y[2]:end position
 
+# Check if there is an overlapping region between the coordinates of two DNA sequences (SAM, GCF)
 def in_or_not(x,y):
     if x[1] >= y[1] and x[1] <=y[2]:
         return True
@@ -40,35 +42,40 @@ def in_or_not(x,y):
 
 tot  = 0
 
+# Open and read the SAM file
 sam = open('Path_of_alignment_1mismatch_aligned_only_sam.txt', 'r') # Replace 'Path_of_alignment_1mismatch_aligned_only_sam.txt' with your SAM file path
 
-for _ in range(3):
-    sam.readline()
-for _ in sam:
-    tot += 1
-
-sam = open('Path_of_alignment_1mismatch_aligned_only_sam.txt', 'r') # Replace 'Path_of_alignment_1mismatch_aligned_only_sam.txt' with your SAM file path
-
+# Remove the first three lines of unnecessary information from a SAM file before running the program 
 for _ in range(3):
     sam.readline()
     
-cnt = 0
-
+#
 A = 0 # (The number of reads that target the non-template strand in the SAM file)
 B = 0 # (The number of reads that target the template strand in the SAM file)
 C = 0 # (The number of reads in the SAM file)
 
+# Perform the following operations on every line of the SAM file
 while True:
-    cnt += 1
     sam_temp = sam.readline()
+
+    # Terminate the while loop when the SAM file has been read completely
     if sam_temp == '':
         break
+        
     sam_seq = sam_mining(sam_temp)
+
+    # Open and read the GCF file
     gcf = open('Path_of_GCF_file.txt', 'r') # Replace 'Path_of_GCF_file.txt' with your GCF file path
+    
+    # Remove the first three lines of unnecessary information from a GCF file before running the program  
     for _ in range(3):
         gcf.readline()
+        
+    # Perform the following operations on every line of the GCF file
     while True:
         get_temp = gcf.readline()
+
+        # Terminate the while loop when the GCF file has been read completely
         if get_temp == '###\n':
             break
         else:
@@ -90,5 +97,6 @@ while True:
 
 print(A,B,A/(A+B),C)
 
+# Close the opened SAM file and GCF file after reading them
 sam.close()
 gcf.close()
